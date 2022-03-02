e22a673dda6b2ddbf1ba902c6df79376dc81e40e
Ivan Onchuru <ivan.onchuru@attain-es.com>
2022-03-03 02:03:00 +0300

    First Commit

---
+++ MinimalAPITutorial/DataContext.cs
@@ -0,0 +1,16 @@
+﻿global using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace MinimalAPITutorial
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+        
+    }
+}
---
+++ MinimalAPITutorial/SuperHero.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalAPITutorial
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string HeroName { get; set; } = string.Empty;
+    }
+}
